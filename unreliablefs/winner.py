48cfecde01cb479ff8708628914ab73a0cec38a4
a-raghavan <akshayraghavan@gmail.com>
2023-02-25 18:58:38 -0600

    winner.py fix

--- unreliablefs/winner.py
+++ unreliablefs/winner.py
@@ -1,6 +1,6 @@
 import time
 
-with open("/users/Girish/fusemnt/winner", "w+") as f:
+with open("/users/Girish/fusemnt/winner", "w") as f:
     #f.write("No winner yet!")
     f.flush()
 
@@ -13,15 +13,16 @@ msgsz_set = [1200*i for i in range(1,11)] # in bytes
 #msgsz_set = [15]
 
 # start expt
+j = 1
 for msgsz in msgsz_set:
-    for i in range(1,21):
+    for trial in range(1,21):
         # start each round @ approx fixed times
         # this will help us find who the winner is in each round
-        while time.time() < start_time+cooldown_sec*i: continue
-        print(time.time())
+        while time.time() < start_time+cooldown_sec*j: continue
         print(msgsz)
         f = open(common_file, "w")
         msg = "a" * msgsz
         f.write(msg)
         f.flush()
         f.close()
+        j += 1
